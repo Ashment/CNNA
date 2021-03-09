82b1f4987ce60af30ef5d4ed2a09f10a74a608ba
David Hou <gundamme@gmail.com>
2021-03-09 18:53:38 -0500

    Now has HP AXI enabled

--- hardware/HLS/cnna.cpp
+++ hardware/HLS/cnna.cpp
@@ -1,25 +1,70 @@
 #include "cnna.h"
 #include <string.h>
 
-void cnna(int8 in_data[3][3], int8 in_weights[3][3][3], int8& out_data)
+void cnna(int8 in_data[3], int8 in_weights[3], int8 out[3])
 {
-#pragma HLS INTERFACE s_axilite port=in_data
-#pragma HLS INTERFACE s_axilite port=in_weights
-#pragma HLS INTERFACE s_axilite port=out_data
-#pragma HLS INTERFACE ap_ctrl_none port=return
+#pragma HLS INTERFACE m_axi 		depth=3 	offset=slave	port=in_data		bundle=INPUT
+#pragma HLS INTERFACE m_axi 		depth=3		offset=slave	port=in_weights		bundle=INPUT
+#pragma HLS INTERFACE m_axi			depth=3		offset=slave	port=out			bundle=OUTPUT
+#pragma HLS INTERFACE s_axilite register port=return
 
-int8 i = 0;
-int8 j, k;
-int8 acc = 0;
+	int i,j;
 
-// Does this infer into DSP slice as a MAC? Probably.
-for(j=0; j<9; j++){
-	for(k=0; k<9; k++){
-		#pragma HLS unroll
-		acc += in_data[j][k] * in_weights[i][j][k];
+	// Move data from AXI to buffer
+	int8 dbuf[3];
+	int8 wbuf[3];
+	int8 obuf[3];
+	for(i=0; i<3; i++){
+		dbuf[i] = in_data[i];
 	}
+	for(i=0; i<3; i++){
+		wbuf[i] = in_weights[i];
+	}
+
+	// Do some operation
+	for(i=0; i<3; i++){
+		obuf[i] = 0;
+		for(j=0; j<3; j++){
+#pragma HLS pipeline
+			obuf[i] += dbuf[i] * wbuf[j];
+		}
+	}
+
+	// Copy from buffer to output
+	for(i=0; i<3; i++){
+		out[i] = obuf[i];
+	}
+
+	return;
 }
 
-out_data = acc;
-return;
+/*
+void cnna(int8* in_data[3][3], int8* in_weights[3][3][3], int8* out)
+{
+#pragma HLS INTERFACE m_axi 		depth=3*3 		offset=slave	port=in_data
+#pragma HLS INTERFACE m_axi 		depth=3*3*3		offset=slave	port=in_weights
+#pragma HLS INTERFACE m_axi							offset=slave	port=out
+#pragma HLS INTERFACE s_axilite port=return
+
+	int8 i = 0;
+	int8 j, k;
+	int8 acc = 0;
+
+	// Move data from AXI to buffer
+	int8 dbuf[3][3];
+	int8 wbuf[3][3][3];
+	memcpy(dbuf, (const int8*) in_data, (3*3)*sizeof(int8));
+	memcpy(wbuf, (const int8*) in_weights, (3*3*3)*sizeof(int8));
+
+	// Does this infer into DSP slice as a MAC? Probably.
+	for(j=0; j<9; j++){
+		for(k=0; k<9; k++){
+			#pragma HLS unroll
+			acc += dbuf[j][k] * wbuf[i][j][k];
+		}
+	}
+
+	*out = acc;
+	return;
 }
+*/
