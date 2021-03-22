f068acb7f6ad788e685b01f8452f4a90594c2fec
David Hou <gundamme@gmail.com>
2021-03-22 15:03:04 -0400

    New overlay for testing FxP

--- hardware/HLS/cnna.cpp
+++ hardware/HLS/cnna.cpp
@@ -1,44 +1,48 @@
 #include "cnna.h"
 #include <string.h>
 
-void cnna(int8 in_data[3][3], int8 in_weights[3][3], int8 out[3])
+void cnna(FIX_FM in_data[3][3][3], FIX_WT in_weights[3][3][3], FIX_FM out[3][3])
 {
-#pragma HLS INTERFACE m_axi 		depth=3*3 		offset=slave	port=in_data		bundle=INPUT
-#pragma HLS INTERFACE m_axi 		depth=3*3		offset=slave	port=in_weights		bundle=INPUT
-#pragma HLS INTERFACE m_axi			depth=3			offset=slave	port=out			bundle=OUTPUT
+#pragma HLS INTERFACE m_axi 		depth=3*3*3 		offset=slave	port=in_data		bundle=INPUT
+#pragma HLS INTERFACE m_axi 		depth=3*3*3			offset=slave	port=in_weights		bundle=INPUT
+#pragma HLS INTERFACE m_axi			depth=3*3			offset=slave	port=out			bundle=OUTPUT
 #pragma HLS INTERFACE s_axilite register port=return
 
-	int8 i,j;
+	int8 i,j,k;
 
 	// Move data from AXI to buffer, initialize output buffer
-	int8 dbuf[3][3];
-	int8 wbuf[3][3];
-	int8 obuf[3];
+	FIX_FM dbuf[3][3][3];
+	FIX_WT wbuf[3][3][3];
+	FIX_FM obuf[3][3];
+
 	for(i=0; i<3; i++){
 		for(j=0; j<3; j++){
-			#pragma HLS unroll
-			dbuf[i][j] = in_data[i][j];
-		}
-	}
-	for(i=0; i<3; i++){
-		for(j=0; j<3; j++){
-			#pragma HLS unroll
-			wbuf[i][j] = in_weights[i][j];
+			for(k=0; k<3; k++){
+				#pragma HLS pipeline
+				dbuf[i][j][k] = in_data[i][j][k];
+				wbuf[i][j][k] = in_weights[i][j][k];
+			}
 		}
 	}
 
 	// Do operations
 	for(i=0; i<3; i++){
-		obuf[i] = 0;
 		for(j=0; j<3; j++){
-			obuf[i] += dbuf[i][j] * wbuf[i][j];
+			#pragma HLS pipeline
+			obuf[i][j] = 0;
+			for(k=1; k<3; k++){
+				#pragma HLS unroll
+				obuf[i][j] += dbuf[i][j][k] * wbuf[i][j][k];
+			}
 		}
 	}
 
 	// Copy from buffer to output
 	for(i=0; i<3; i++){
-		#pragma HLS unroll
-		out[i] = obuf[i];
+		for(j=0; j<3; j++){
+			#pragma HLS pipeline
+			out[i][j] = obuf[i][j];
+		}
 	}
 
 	return;
