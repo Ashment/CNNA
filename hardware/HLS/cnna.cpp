51b521bcd0575e80c900531a75cc79010ae8e640
David Hou <gundamme@gmail.com>
2021-04-13 20:08:26 -0400

    Changed some pipeline and unrolls. Also fixed top-level function output.

--- hardware/HLS/cnna.cpp
+++ hardware/HLS/cnna.cpp
@@ -7,7 +7,7 @@ void CONV3X3(FIX_FM in_fm[3][3], FIX_WT in_wt[3][3], FIX_FM *out){
 
 	for(int i=0; i<3; i++){
 		for(int j=0; j<3; j++){
-			#pragma HLS pipeline
+			#pragma HLS unroll factor=9
 			vout += in_fm[i][j] * in_wt[i][j];
 		}
 	}
@@ -17,14 +17,14 @@ void CONV3X3(FIX_FM in_fm[3][3], FIX_WT in_wt[3][3], FIX_FM *out){
 }
 
 void CONVDEPTH32(FIX_FM in_fm[32][3][3], FIX_WT in_wt[32][3][3], FIX_FM *out){
-	// For convolution of volume of 32x3x3
+	// For convolution volume of 32x3x3
 	FIX_FM vout = 0;
 	FIX_FM vbuf;
 
 	for(int i=0; i<32; i++){
-		#pragma HLS unroll factor=4
-		// (i) for each input channel.
+		// (i) for each depth in IFM
 		CONV3X3(in_fm[i], in_wt[i], &vbuf);
+		vout += vbuf;
 	}
 
 	*out = vout;
@@ -36,28 +36,23 @@ void CONVL2(FIX_FM in_fm[32][32][32], FIX_WT in_wt[32][32][3][3], FIX_FM out_fm[
 	// First CONV layer. No buffer for out_fm. Padding=1 for same padding
 	// (32x32x32) INPUT | 32 Channels 3x3 | (32x32x32) OUTPUT
 
-	FIX_FM fmBuf[32][3][3];
-	FIX_WT wtBuf[32][3][3];
-	FIX_FM tout;
-
 	// Explicitly zero output buffer... (placeholder until padding is working)
 	for(int i=0; i<32; i++){
 		for(int j=0; j<32; j++){
 			for(int k=0; k<32; k++){
-				#pragma HLS loop_flatten
 				#pragma HLS pipeline
 				out_fm[i][j][k] = 0;
-
 			}
 		}
 	}
 
-	// Aiming for output stationary kind of array.
+	// Aiming for output stationary PUs. Kinda, sorta.
 	// (ch,j,k) is output indices. Load the IFM only once for each CONV frame, all channels
 	for(int i=0; i<32; i++){
 		for(int j=1; j<31; j++){
 			for(int k=1; k<31; k++){
 				// Populate IFM buffer.
+				FIX_FM fmBuf[32][3][3];
 				for(int jj=0; jj<3; jj++){
 					for(int kk=0; kk<3; kk++){
 						#pragma HLS pipeline
@@ -67,9 +62,10 @@ void CONVL2(FIX_FM in_fm[32][32][32], FIX_WT in_wt[32][32][3][3], FIX_FM out_fm[
 
 				// populate weights for given channel and compute output
 				for(int ch=0; ch<32; ch++){
+					#pragma HLS pipeline
+					FIX_WT wtBuf[32][3][3];
 					for(int jj=0; jj<3; jj++){
 						for(int kk=0; kk<3; kk++){
-							#pragma HLS pipeline
 							wtBuf[i][jj][kk] = in_wt[ch][i][j+jj-1][k+kk-1];
 						}
 					}
@@ -87,8 +83,6 @@ void cnna(FIX_FM in_data[32][32][32], FIX_WT in_weights[32][32][3][3], FIX_FM ou
 #pragma HLS INTERFACE m_axi			depth=32*32*32		offset=slave	port=out			bundle=OUTPUT
 #pragma HLS INTERFACE s_axilite register port=return
 
-#pragma HLS ALLOCATION function instances=CONV3X3 limit=3
-
 	// Define input and output buffers
 	FIX_FM dbuf[32][32][32];
 	FIX_WT wbuf[32][32][3][3];
@@ -98,7 +92,6 @@ void cnna(FIX_FM in_data[32][32][32], FIX_WT in_weights[32][32][3][3], FIX_FM ou
 	for(int i=0; i<32; i++){
 		for(int j=0; j<32; j++){
 			for(int k=0; k<32; k++){
-				#pragma HLS loop_flatten
 				#pragma HLS pipeline
 				dbuf[i][j][k] = in_data[i][j][k];
 
@@ -109,9 +102,8 @@ void cnna(FIX_FM in_data[32][32][32], FIX_WT in_weights[32][32][3][3], FIX_FM ou
 		for(int i=0; i<32; i++){
 			for(int j=0; j<3; j++){
 				for(int k=0; k<3; k++){
-					#pragma HLS loop_flatten
 					#pragma HLS pipeline
-					wbuf[ch][i][j][k] = in_weights[ch][i][j][k]
+					wbuf[ch][i][j][k] = in_weights[ch][i][j][k];
 				}
 			}
 		}
@@ -121,10 +113,12 @@ void cnna(FIX_FM in_data[32][32][32], FIX_WT in_weights[32][32][3][3], FIX_FM ou
 	CONVL2(dbuf, wbuf, obuf);
 
 	// Copy from buffer to output
-	for(int i=0; i<3; i++){
-		for(int j=0; j<3; j++){
-			#pragma HLS pipeline
-			out[i] = obuf[i];
+	for(int i=0; i<32; i++){
+		for(int j=0; j<32; j++){
+			for(int k=0; k<32; k++){
+				#pragma HLS pipeline
+				out[i][j][k] = obuf[i][j][k];
+			}
 		}
 	}
 
--- hardware/HLS/cnna.h
+++ hardware/HLS/cnna.h
@@ -1,7 +1,11 @@
 #include "ap_axi_sdata.h"
 
 //#define CSIM_DEBUG
-// NN Data Types
+
+//////////////////
+//  Data Types  //
+//////////////////
+
 #ifdef CSIM_DEBUG
 	typedef int uint8;
 	typedef int int8;
@@ -16,28 +20,12 @@
 	typedef ap_fixed<8, 1, AP_TRN_ZERO, AP_SAT> FIX_WT; 	//Fixed Point for Weights
 #endif
 
+///////////////////////////
+//  Classes and Methods  //
+///////////////////////////
+
+class CONV3X3PU;
+
 void cnna(FIX_FM in_data[3][3][3], FIX_WT in_weights[3][3][3], FIX_FM out[3]);
-void CONV3X3(FIX_FM in_fm[3][3], FIX_WT in_wt[3][3], FIX_FM *out);
 void CONVDEPTH32(FIX_FM in_fm[32][3][3], FIX_WT in_wt[32][3][3], FIX_FM *out);
 void CONVL2(FIX_FM in_fm[32][32][32], FIX_WT in_wt[32][32][3][3], FIX_FM out_fm[32][32][32]);
-
-/*
-#ifdef CSIM_DEBUG
-	typedef float FIX_32_4;	//fix point
-	typedef float FIX_32_25;	//fix point
-	typedef float FIX_FM;	//fix point for feature map
-	typedef float FIX_WT;	//fix point for weights
-	typedef float FIX_32_16;	//fix point
-	typedef float FIX_32_12;	//fix point
-	typedef float FIX_16_1;
-#else
-	typedef ap_fixed<16, 6, AP_TRN_ZERO, AP_SAT> FIX_FM;	//fix point for feature map
-	typedef ap_fixed<8,  1, AP_TRN_ZERO, AP_SAT> FIX_WT;	//fix point for weights
-	typedef ap_fixed<16, 1, AP_TRN_ZERO, AP_SAT> FIX_16_1;	//fix point for weights
-	typedef ap_fixed<32,16, AP_TRN_ZERO, AP_SAT> FIX_32_16;	//fix point
-	typedef ap_fixed<32,12, AP_TRN_ZERO, AP_SAT> FIX_32_12;
-	typedef ap_fixed<32, 4, AP_TRN_ZERO, AP_SAT> FIX_32_4;	//fix point
-	typedef ap_fixed<32,25, AP_TRN_ZERO, AP_SAT> FIX_32_25;	//fix point
-#endif
-*/
-
