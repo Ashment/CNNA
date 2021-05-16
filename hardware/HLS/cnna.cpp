cf762f0f6f5b7f8143839f0781eb5d79cc4551fc
David Hou <gundamme@gmail.com>
2021-05-16 14:42:31 -0400

    Tested pragma directives. Now to test manual code transformations.

--- hardware/HLS/cnna.cpp
+++ hardware/HLS/cnna.cpp
@@ -165,7 +165,7 @@ void L1DPU(FIX_FM in_fm[3][34][34], FIX_WT in_wt[32][3][3][3], int anchor[3], FI
 void CONVL2(FIX_FM in_fm[32][32][32], FIX_WT in_wt[32][32][3][3], FIX_FM out_fm[32][32][32]){
 	// CONV layer. No buffer for out_fm. Padding=1 for same padding
 	// (32x32x32) INPUT | 32 Channels 3x3 | (32x32x32) OUTPUT
-	#pragma HLS ALLOCATION function instances=L2DPU limit=32
+	#pragma HLS ALLOCATION function instances=L2DPU limit=256
 
 	FIX_WT wt_bufL2[32][32][3][3];
 	#pragma HLS ARRAY_PARTITION variable=wt_bufL2 complete dim=1
@@ -182,7 +182,7 @@ void CONVL2(FIX_FM in_fm[32][32][32], FIX_WT in_wt[32][32][3][3], FIX_FM out_fm[
 
 	// Buffer input FM with padding
 	FIX_FM in_padded[32][34][34];
-	#pragma HLS ARRAY_PARTITION variable=in_padded complete dim=1
+	#pragma HLS ARRAY_PARTITION variable=in_padded complete
 	for(int ch=0; ch<32; ch++){
 		// Padding
 		for(int i=0; i<34; i++){
@@ -203,11 +203,24 @@ void CONVL2(FIX_FM in_fm[32][32][32], FIX_WT in_wt[32][32][3][3], FIX_FM out_fm[
 
 	for(int ch=0; ch<32; ch++){
 		for(int j=0; j<32; j++){
+			#pragma HLS unroll factor=8
 			for(int k=0; k<32; k++){
-				#pragma HLS unroll factor=32
-				#pragma HLS pipeline
-				int cAnchor[3] = {ch, j, k};
+				//#pragma HLS loop_flatten
+				//#pragma HLS unroll factor=32
+				int cAnchor[3] = {ch, j, (k)};
 				L2DPU(in_padded, wt_bufL2, cAnchor, &out_fm[ch][j][k]);
+
+				/*
+				// Testing 4DPU per innermost iteration and unrolling intermediate loop
+				int cAnchor1[3] = {ch, j, (4*k)};
+				L2DPU(in_padded, wt_bufL2, cAnchor1, &out_fm[ch][j][(4*k)]);
+				int cAnchor2[3] = {ch, j, (4*k)+1};
+				L2DPU(in_padded, wt_bufL2, cAnchor2, &out_fm[ch][j][(4*k)+1]);
+				int cAnchor3[3] = {ch, j, (4*k)+2};
+				L2DPU(in_padded, wt_bufL2, cAnchor3, &out_fm[ch][j][(4*k)+2]);
+				int cAnchor4[3] = {ch, j, (4*k)+3};
+				L2DPU(in_padded, wt_bufL2, cAnchor4, &out_fm[ch][j][(4*k)+3]);
+				*/
 			}
 		}
 	}
@@ -230,7 +243,7 @@ void L2DPU(FIX_FM in_fm[32][34][34], FIX_WT in_wt[32][32][3][3], int anchor[3], 
 	for(int i=0; i<32; i++){
 		for(int j=0; j<3; j++){
 			for(int k=0; k<3; k++){
-				#pragma HLS pipeline
+				//#pragma HLS pipeline
 				w_buf[i][j][k] = in_wt[anchor[0]][i][j][k];
 				d_buf[i][j][k] = in_fm[i][anchor[1]+j][anchor[2]+k];
 			}
@@ -243,7 +256,7 @@ void L2DPU(FIX_FM in_fm[32][34][34], FIX_WT in_wt[32][32][3][3], int anchor[3], 
 		CONV3X3(d_buf[i], w_buf[i], &vbuf[i]);
 	}
 	for(int i=0; i<32; i++){
-		#pragma HLS pipeline
+		//#pragma HLS pipeline
 		obuf += vbuf[i];
 	}
 	*out = (obuf >= FIX_FM(0)) ? obuf : FIX_FM(0);
@@ -255,7 +268,7 @@ void CONV3X3(FIX_FM in_fm[3][3], FIX_WT in_wt[3][3], FIX_FM *out){
 	FIX_FM vout = 0;
 	for(int i=0; i<3; i++){
 		for(int j=0; j<3; j++){
-			#pragma HLS pipeline
+			//#pragma HLS pipeline
 			vbuf = in_fm[i][j] * in_wt[i][j];
 			vout += vbuf;
 		}
